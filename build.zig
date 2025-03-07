be5fa3876d81780fcb9cd3b291117da3ce0832d8
Léon van der Kaap <leonkaap@gmail.com>
2025-03-07 18:44:15 +0100

    Set Zig compatibility to 0.14

--- build.zig
+++ build.zig
@@ -1,29 +1,42 @@
 const std = @import("std");
-const Builder = std.Build;
 const version = @import("version.zig");
 const builtin = std.builtin;
 
+const Step = std.Build.Step;
+const Builder = std.Build;
+
 pub fn build(b: *Builder) void {
     const current_zig_version = @import("builtin").zig_version;
-    if (current_zig_version.major != 0 or current_zig_version.minor < 13) {
-        std.debug.print("This project does not compile with a Zig version <0.13.x. Exiting.", .{});
+    if (current_zig_version.major != 0 or current_zig_version.minor < 14) {
+        std.debug.print("This project does not compile with a Zig version <0.14.x. Exiting.", .{});
         std.os.exit(1);
     }
 
+    const target = b.standardTargetOptions(.{});
+
     const lib = b.addStaticLibrary(.{
-        .name = "hashes",
+        .name = "crypto",
         .root_source_file = b.path("src/std.zig"),
         .optimize = .ReleaseSafe,
         .version = .{.major = version.major, .minor = version.minor, .patch = version.patch},
-        .target = b.host
-        });
-        b.installArtifact(lib);
+        .target = target
+    });
+    b.installArtifact(lib);
 
-
-    var main_tests = b.addTest(.{ .root_source_file = b.path("src/gost.zig")});
-    _ = b.addTest(.{ .root_source_file = b.path("src/has160.zig")});
-    _ = b.addTest(.{ .root_source_file = b.path("src/md4.zig")});
+    const main_tests = addTests(b, &.{
+        .{.root_source_file = b.path("src/has160.zig")},
+        .{.root_source_file = b.path("src/md4.zig")},
+    });
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
+
+}
+
+fn addTests(b: *Builder, tests: []const std.Build.TestOptions) *Step.Compile {
+    var result: *Step.Compile = undefined;
+    for (tests) |testElem| {
+        result = b.addTest(testElem);
+    }
+    return result;
 }
